49c282bd29fd7ff0f61794ffb1a16368fc155d10
Okihita <okihita@gmail.com>
2015-05-02 17:40:23 +0700

    Graph code is neat!

--- app/src/main/java/com/okihita/glutracker/ViewBase/GraphFragment.java
+++ app/src/main/java/com/okihita/glutracker/ViewBase/GraphFragment.java
@@ -4,7 +4,6 @@ import android.app.Fragment;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -25,7 +24,6 @@ import com.okihita.glutracker.util.GraphValueFormatter;
 
 import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.JSONObject;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -38,11 +36,8 @@ public class GraphFragment extends Fragment {
     private BarChart mChartPostmeal;
     private BarChart mChartRandom;
     private ArrayList<MeasurementItem> mMeasurementItems = new ArrayList<>();
-    private ArrayList<MeasurementItem> mUniqueDatePREMEALMeasurementItems = new ArrayList<>();
-    private ArrayList<MeasurementItem> mUniqueDatePOSTMEALMeasurementItems = new ArrayList<>();
-    private ArrayList<MeasurementItem> mUniqueDateRANDOMMeasurementItems = new ArrayList<>();
 
-    private SimpleDateFormat mysdf = (new SimpleDateFormat("dd MMMM", new Locale("id", "ID")));
+    private SimpleDateFormat sdf = (new SimpleDateFormat("dd MMMM", new Locale("id", "ID")));
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -59,7 +54,6 @@ public class GraphFragment extends Fragment {
         mChartPostmeal = (BarChart) view.findViewById(R.id.bar_chart_postmeal);
         mChartRandom = (BarChart) view.findViewById(R.id.bar_chart_random);
 
-        /* Basic chart settings. s*/
         prepareChart(mChartPremeal);
         prepareChart(mChartPostmeal);
         prepareChart(mChartRandom);
@@ -73,106 +67,48 @@ public class GraphFragment extends Fragment {
         chart.getAxisRight().setEnabled(false);
     }
 
+    private void specificChartSet(int measurementMode, BarChart chart, String label) {
+
+        /* Make an array list containing ONLY data from a mode. */
+        ArrayList<MeasurementItem> tempMeasurementItems = new ArrayList<>();
+        String tempDate = sdf.format(mMeasurementItems.get(0).getTanggalAmbil());
+
+        for (int i = 0; i < mMeasurementItems.size(); i++) {
+            String nowDate = sdf.format(mMeasurementItems.get(i).getTanggalAmbil());
+            if (!nowDate.equals(tempDate) && mMeasurementItems.get(i).getJenis() == measurementMode) {
+                tempMeasurementItems.add(mMeasurementItems.get(i));
+                tempDate = nowDate;
+            }
+        }
+
+        /* Set the y-axis and x-axis values. */
+        ArrayList<String> xVals = new ArrayList<>();
+        ArrayList<BarEntry> yVals = new ArrayList<>();
+
+        for (int i = 0; i < tempMeasurementItems.size(); i++) {
+            xVals.add(sdf.format(tempMeasurementItems.get(i).getTanggalAmbil()));
+            yVals.add(new BarEntry(tempMeasurementItems.get(i).getKadar(), i));
+        }
+
+        /* Initiate, hook, and refresh the datasets. */
+        BarDataSet dataSet = new BarDataSet(yVals, label);
+        dataSet.setBarSpacePercent(35f);
+        dataSet.setValueFormatter(new GraphValueFormatter());
+
+        ArrayList<BarDataSet> dataSets = new ArrayList<>();
+        dataSets.add(dataSet);
+
+        BarData barData = new BarData(xVals, dataSets);
+        barData.setValueTextSize(10f);
+        chart.setData(barData);
+
+        chart.invalidate();
+    }
+
     private void setChartData() {
-        /* TIAP INDIVIDU */
-        // PREMEAL
-        String tanggalTempPremeal = (mysdf).format(mMeasurementItems.get(0).getTanggalAmbil());
-        for (int i = 0; i < mMeasurementItems.size(); i++) {
-            String tanggalSekarang = (mysdf).format(mMeasurementItems.get(i).getTanggalAmbil());
-            if (!tanggalSekarang.equals(tanggalTempPremeal) && mMeasurementItems.get(i).getJenis() == Config.MEASUREMENT_MODE_PREMEAL) {
-                mUniqueDatePREMEALMeasurementItems.add(mMeasurementItems.get(i));
-                tanggalTempPremeal = tanggalSekarang;
-                Log.d(Config.TAG, "Tanggal " + tanggalSekarang + " dengan tipe " + mMeasurementItems.get(i).getJenisTeks()
-                        + " masuk. Nilai: " + mMeasurementItems.get(i).getKadar());
-            }
-        }
-
-        // Initiating X-values and Y-Values for premeal
-        ArrayList<String> xValsPremeal = new ArrayList<>();
-        ArrayList<BarEntry> yValsPremeal = new ArrayList<>();
-        for (int i = 0; i < mUniqueDatePREMEALMeasurementItems.size(); i++) {
-            Log.d(Config.TAG, "Nomor " + i + " kadarnya: " + mUniqueDatePREMEALMeasurementItems.get(i).getKadar());
-            xValsPremeal.add((new SimpleDateFormat("dd MMMM", Locale.ENGLISH)).format(mUniqueDatePREMEALMeasurementItems.get(i).getTanggalAmbil()));
-            yValsPremeal.add(new BarEntry(mUniqueDatePREMEALMeasurementItems.get(i).getKadar(), i));
-        }
-
-        /* Buat sebuah set: PREMEAL. */
-        BarDataSet datasetPremeal = new BarDataSet(yValsPremeal, "Premeal Result");
-        datasetPremeal.setBarSpacePercent(35f);
-        datasetPremeal.setValueFormatter(new GraphValueFormatter());
-        ArrayList<BarDataSet> dataSetsPremeal = new ArrayList<>();
-        dataSetsPremeal.add(datasetPremeal);
-        BarData premealBarData = new BarData(xValsPremeal, dataSetsPremeal);
-        premealBarData.setValueTextSize(10f);
-
-        mChartPremeal.setData(premealBarData);
-
-
-        // POSTMEAL
-        String tanggalTempPostmeal = (mysdf).format(mMeasurementItems.get(0).getTanggalAmbil());
-        for (int i = 0; i < mMeasurementItems.size(); i++) {
-            String tanggalSekarang = (mysdf).format(mMeasurementItems.get(i).getTanggalAmbil());
-            if (!tanggalSekarang.equals(tanggalTempPostmeal) && mMeasurementItems.get(i).getJenis() == Config.MEASUREMENT_MODE_POSTMEAL) {
-                mUniqueDatePOSTMEALMeasurementItems.add(mMeasurementItems.get(i));
-                tanggalTempPostmeal = tanggalSekarang;
-                Log.d(Config.TAG, "Tanggal " + tanggalSekarang + " dengan tipe " + mMeasurementItems.get(i).getJenisTeks()
-                        + " masuk. Nilai: " + mMeasurementItems.get(i).getKadar());
-            }
-        }
-
-        // Initiating X-values and Y-Values for postmeal
-        ArrayList<String> xValsPostmeal = new ArrayList<>();
-        ArrayList<BarEntry> yValsPostmeal = new ArrayList<>();
-        for (int i = 0; i < mUniqueDatePOSTMEALMeasurementItems.size(); i++) {
-            Log.d(Config.TAG, "Nomor " + i + " kadarnya: " + mUniqueDatePOSTMEALMeasurementItems.get(i).getKadar());
-            xValsPostmeal.add((mysdf).format(mUniqueDatePOSTMEALMeasurementItems.get(i).getTanggalAmbil()));
-            yValsPostmeal.add(new BarEntry(mUniqueDatePOSTMEALMeasurementItems.get(i).getKadar(), i));
-        }
-
-
-        // RANDOM
-        String tanggalTempRandom = (mysdf).format(mMeasurementItems.get(0).getTanggalAmbil());
-        for (int i = 0; i < mMeasurementItems.size(); i++) {
-            String tanggalSekarang = (mysdf).format(mMeasurementItems.get(i).getTanggalAmbil());
-            if (!tanggalSekarang.equals(tanggalTempRandom) && mMeasurementItems.get(i).getJenis() == Config.MEASUREMENT_MODE_RANDOM) {
-                mUniqueDateRANDOMMeasurementItems.add(mMeasurementItems.get(i));
-                tanggalTempRandom = tanggalSekarang;
-                Log.d(Config.TAG, "Tanggal " + tanggalSekarang + " dengan tipe " + mMeasurementItems.get(i).getJenisTeks()
-                        + " masuk. Nilai: " + mMeasurementItems.get(i).getKadar());
-            }
-        }
-
-        // Initiating X-values and Y-Values for random
-        ArrayList<String> xValsRandom = new ArrayList<>();
-        ArrayList<BarEntry> yValsRandom = new ArrayList<>();
-        for (int i = 0; i < mUniqueDateRANDOMMeasurementItems.size(); i++) {
-            Log.d(Config.TAG, "Nomor " + i + " kadarnya: " + mUniqueDateRANDOMMeasurementItems.get(i).getKadar());
-            xValsRandom.add((mysdf).format(mUniqueDateRANDOMMeasurementItems.get(i).getTanggalAmbil()));
-            yValsRandom.add(new BarEntry(mUniqueDateRANDOMMeasurementItems.get(i).getKadar(), i));
-        }
-
-
-        /* Buat sebuah set: POSTMEAL. */
-        BarDataSet datasetPostmeal = new BarDataSet(yValsPostmeal, "Postmeal Result");
-        datasetPostmeal.setBarSpacePercent(35f);
-        datasetPostmeal.setValueFormatter(new GraphValueFormatter());
-        ArrayList<BarDataSet> dataSetsPostmeal = new ArrayList<>();
-        dataSetsPostmeal.add(datasetPostmeal);
-        BarData postmealBarData = new BarData(xValsPostmeal, dataSetsPostmeal);
-        postmealBarData.setValueTextSize(10f);
-
-        /* Buat sebuah set: RANDOM. */
-        BarDataSet datasetRandom = new BarDataSet(yValsRandom, "Random Result");
-        datasetRandom.setBarSpacePercent(35f);
-        datasetRandom.setValueFormatter(new GraphValueFormatter());
-        ArrayList<BarDataSet> dataSetsRandom = new ArrayList<>();
-        dataSetsRandom.add(datasetRandom);
-        BarData randomBarData = new BarData(xValsRandom, dataSetsRandom);
-        randomBarData.setValueTextSize(10f);
-
-        /* Attach datasets to charts. */
-        mChartPostmeal.setData(postmealBarData);
-        mChartRandom.setData(randomBarData);
+        specificChartSet(Config.MEASUREMENT_MODE_PREMEAL, mChartPremeal, "Premeal Chart");
+        specificChartSet(Config.MEASUREMENT_MODE_POSTMEAL, mChartPostmeal, "Postmeal Chart");
+        specificChartSet(Config.MEASUREMENT_MODE_RANDOM, mChartRandom, "Random Chart");
     }
 
     /**
@@ -183,27 +119,18 @@ public class GraphFragment extends Fragment {
         Uri.Builder mBaseUriBuilder = (new Uri.Builder()).scheme("http")
                 .authority(Config.BASE_URL)
                 .appendPath(Config.SUBDOMAIN_ADDRESS);
-        String graphFragmentQuery = mBaseUriBuilder.build().toString();
+        String graphMeasurementDataQuery = mBaseUriBuilder.build().toString();
 
         JsonArrayRequest measurementRequest = new JsonArrayRequest(
-                graphFragmentQuery,
+                graphMeasurementDataQuery,
                 new Response.Listener<JSONArray>() {
-
-                    /* Response. */
-                    @Override
+                   @Override
                     public void onResponse(JSONArray response) {
                         try {
                             mMeasurementItems.clear();
-                            for (int i = 0; i < response.length(); i++) {
-                                JSONObject object = response.getJSONObject(i);
-                                MeasurementItem item = new MeasurementItem(object);
-                                mMeasurementItems.add(item);
-                            }
-
+                            for (int i = 0; i < response.length(); i++)
+                                mMeasurementItems.add(new MeasurementItem(response.getJSONObject(i)));
                             setChartData();
-                            mChartPremeal.invalidate();
-                            mChartPostmeal.invalidate();
-                            mChartRandom.invalidate();
                         } catch (JSONException ignored) {
                         }
                     }
--- app/src/main/java/com/okihita/glutracker/model/MeasurementItem.java
+++ app/src/main/java/com/okihita/glutracker/model/MeasurementItem.java
@@ -31,7 +31,6 @@ public class MeasurementItem {
         }
     }
 
-
     public int getId() {
         return mId;
     }
